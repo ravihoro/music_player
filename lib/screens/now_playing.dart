b9683dc395d1bced00f48265f8622363eccd3681
ravihoro <ravijohnhoro5@gmail.com>
2020-11-02 14:55:30 +0530

    Modified now playing screen

--- lib/screens/now_playing.dart
+++ lib/screens/now_playing.dart
@@ -9,6 +9,8 @@ class NowPlaying extends StatefulWidget {
 }
 
 class _NowPlayingState extends State<NowPlaying> {
+  double sliderValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,18 +24,122 @@ class _NowPlayingState extends State<NowPlaying> {
         ),
       ),
       backgroundColor: Colors.grey[850],
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Consumer<SongModel>(
-          builder: (context, songModel, child) {
-            return Text(
-              songModel.currentSong.title,
-              style: TextStyle(color: Colors.white),
-            );
-          },
-        ),
+      body: Consumer<SongModel>(
+        builder: (context, songModel, child) {
+          return Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                songModel.currentSong.title,
+                style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w300),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                songModel.currentSong.artist,
+                style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w300),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(
+                      color: Colors.amber,
+                    ),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        songModel.currentSong.albumArtwork,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Slider(
+                  //divisions: 100,
+                  value: sliderValue,
+                  activeColor: Colors.amber,
+                  onChanged: (value) {
+                    setState(() {
+                      sliderValue = value;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "00:00",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      Duration(
+                              milliseconds:
+                                  int.parse(songModel.currentSong.duration))
+                          .toString()
+                          .substring(2, 7),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.skip_previous),
+                      iconSize: 50,
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.play_arrow),
+                      iconSize: 50,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.skip_next),
+                      color: Colors.white,
+                      iconSize: 50,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
