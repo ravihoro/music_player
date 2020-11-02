627b398483c71246063d455fe85c9b55c8cb5ada
ravihoro <ravijohnhoro5@gmail.com>
2020-11-02 18:49:36 +0530

    Added animation

--- lib/models/song_model.dart
+++ lib/models/song_model.dart
@@ -3,8 +3,14 @@ import 'package:flutter_audio_query/flutter_audio_query.dart';
 
 class SongModel extends ChangeNotifier {
   SongInfo _currentSong;
+  bool _isPlaying = false;
 
   SongInfo get currentSong => _currentSong;
+  bool get isPlaying => _isPlaying;
+
+  void setIsPlaying() {
+    _isPlaying = !_isPlaying;
+  }
 
   void setCurrentSong(SongInfo song) {
     _currentSong = song;
--- lib/screens/now_playing.dart
+++ lib/screens/now_playing.dart
@@ -8,8 +8,24 @@ class NowPlaying extends StatefulWidget {
   _NowPlayingState createState() => _NowPlayingState();
 }
 
-class _NowPlayingState extends State<NowPlaying> {
+class _NowPlayingState extends State<NowPlaying> with TickerProviderStateMixin {
   double sliderValue = 0.0;
+
+  AnimationController _animationController;
+  var isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,18 +68,25 @@ class _NowPlayingState extends State<NowPlaying> {
                 height: 20,
               ),
               Center(
-                child: Container(
-                  height: 200,
-                  width: 200,
-                  decoration: BoxDecoration(
+                child: Card(
+                  elevation: 10,
+                  shadowColor: Colors.amber,
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(100),
-                    border: Border.all(
-                      color: Colors.amber,
-                    ),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        songModel.currentSong.albumArtwork,
+                  ),
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(
+                        color: Colors.amber,
+                      ),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(
+                          songModel.currentSong.albumArtwork,
+                        ),
                       ),
                     ),
                   ),
@@ -112,26 +135,47 @@ class _NowPlayingState extends State<NowPlaying> {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 30,
+              ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     IconButton(
-                      icon: Icon(Icons.skip_previous),
                       iconSize: 50,
+                      icon: Icon(
+                        Icons.skip_previous,
+                      ),
                       color: Colors.white,
                       onPressed: () {},
                     ),
                     IconButton(
-                      icon: Icon(Icons.play_arrow),
                       iconSize: 50,
-                      onPressed: () {},
+                      icon: AnimatedIcon(
+                        icon: AnimatedIcons.play_pause,
+                        progress: _animationController,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          songModel.setIsPlaying();
+                          if (songModel.isPlaying) {
+                            //songModel.setIsPlaying();
+                            _animationController.forward();
+                          } else {
+                            //songModel.setIsPlaying();
+                            _animationController.reverse();
+                          }
+                        });
+                      },
                     ),
                     IconButton(
-                      icon: Icon(Icons.skip_next),
-                      color: Colors.white,
                       iconSize: 50,
+                      icon: Icon(
+                        Icons.skip_next,
+                      ),
+                      color: Colors.white,
                       onPressed: () {},
                     ),
                   ],
